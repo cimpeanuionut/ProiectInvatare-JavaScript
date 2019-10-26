edf55cf0f4a1ce08cee677e4d863c317a5cfa425
cimpeanuionut <43180388+cimpeanuionut@users.noreply.github.com>
2019-10-26 11:45:13 +0300

    version 3

--- app.js
+++ app.js
@@ -58,6 +58,19 @@ var budgetController = (function (){
             return newItem;            
         },
 
+        deleteItem: function(type, id){
+            var ids, index;
+            ids =  data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if(index !== -1){
+                data.allItems[type].splice(index, 1);
+            }
+        },
+
         calculateBudget: function(){
             // Calculate total income and expenses
             calculateTotal('exp');
@@ -142,6 +155,11 @@ var UIController = (function (){
         document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        deleteListItem: function(selectorID){
+            var el =  document.getElementById(selectorID);
+            el.parentNode.removeChild(el); 
+        },
+
         clearFields: function(){
            var fields, fieldsArr
            fields =  document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
@@ -209,13 +227,16 @@ var controller = (function(budgetCtrl, UICtrl) {
         if(itemID){
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             //delete from the data structure
+            budgetCtrl.deleteItem(type, ID);
 
             //delete from UI
+            UICtrl.deleteListItem(itemID);
 
             //update and show the new budget
+            updateBudget();
         }
     };
   
